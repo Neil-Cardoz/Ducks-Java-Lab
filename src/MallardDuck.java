5a328f9b3d28e813c00ce0b63f963d2587d25acf
Neil-Cardoz <149564425+Neil-Cardoz@users.noreply.github.com>
2025-03-08 21:03:02 +0530

    Update MallardDuck.java
    Implemented Mallard Duck

--- src/MallardDuck.java
+++ src/MallardDuck.java
@@ -1,3 +1,12 @@
 public class MallardDuck extends Duck {
-    
+    public MallardDuck() {
+        quackBehaviour = new Quack();
+        swimBehaviour = new Swimming();
+        flyingBehaviour = new Fly();
+
+    }
+    @Override
+    public void display() {
+        System.out.println("I am Mallard Duck");
+    }
 }
