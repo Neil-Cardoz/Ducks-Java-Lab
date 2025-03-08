f163759342921026bdc761ae05d28cd1b37b1d6f
Neil-Cardoz <149564425+Neil-Cardoz@users.noreply.github.com>
2025-03-08 21:03:42 +0530

    Update RedheadDuck.java
    Implemented RedHeadDuck

--- src/RedheadDuck.java
+++ src/RedheadDuck.java
@@ -1,4 +1,12 @@
 public class RedheadDuck extends Duck
 {
-    
+    public RedheadDuck(){ // Constructor
+        quackBehaviour = new Quack();
+        swimBehaviour = new Swimming();
+        flyingBehaviour = new Fly();
+    }
+    @Override
+    public void display() { // method overriding
+        System.out.println("I am a Redhead Duck !!!");
+    }
 }
