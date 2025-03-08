81fa6d4b60e6add0921521212cb0ccb60767ae04
Neil-Cardoz <149564425+Neil-Cardoz@users.noreply.github.com>
2025-03-08 21:05:10 +0530

    Update RubberDuck.java
    Implemented RubberDuck

--- src/RubberDuck.java
+++ src/RubberDuck.java
@@ -1,4 +1,13 @@
 public class RubberDuck extends Duck {
-    
+    public RubberDuck() {
+        quackBehaviour = new Squeak();
+        swimBehaviour = new Floating();
+        flyingBehaviour = new NoFly();
+    }
+
+    @Override
+    public void display() {
+        System.out.println("I'm a rubber duck");
+    }
 
 }
