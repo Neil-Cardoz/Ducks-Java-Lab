6768ab56e915c90dd45593b6423a74ca8c25c8e1
Neil-Cardoz <149564425+Neil-Cardoz@users.noreply.github.com>
2025-03-08 21:05:51 +0530

    Update DecoyDuck.java
    Implemented Decoy Duck

--- src/DecoyDuck.java
+++ src/DecoyDuck.java
@@ -1,4 +1,12 @@
 public class DecoyDuck extends Duck
 {
-    
+    public DecoyDuck(){
+        quackBehaviour = new NoQuack(); //Constructor
+        flyingBehaviour = new NoFly();
+        swimBehaviour = new Sink();
+    }
+    @Override
+    public void display() { // method overriding
+        System.out.println("I am Decoy Duck..");
+    }
 }
