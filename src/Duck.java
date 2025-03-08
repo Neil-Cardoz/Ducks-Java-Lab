1794b58f0a9a96bcd9671efa849037402b24af62
Neil-Cardoz <149564425+Neil-Cardoz@users.noreply.github.com>
2025-03-08 20:56:11 +0530

    Update Duck.java
    Implemented FlyingBehaviour, SwimBehaviour, QuackBehaviour

--- src/Duck.java
+++ src/Duck.java
@@ -3,5 +3,30 @@ abstract public class Duck {
     SwimBehaviour swimBehaviour;
     FlyingBehaviour flyingBehaviour;
 
+    public void setQuackBehaviour(QuackBehaviour behaviour) {
+        this.quackBehaviour = behaviour;
+    }
+
+    public void setSwimBehaviour(SwimBehaviour behaviour) {
+        this.swimBehaviour = behaviour;
+    }
+
+    public void setFlyingBehaviour(FlyingBehaviour behaviour) {
+        this.flyingBehaviour = behaviour;
+    }
+
+    public void performQuack() {
+        quackBehaviour.quack();
+    }
+
+    public void performSwim() {
+        swimBehaviour.swim();
+    }
+    public void performFly() {
+        flyingBehaviour.fly();
+    }
+
+    abstract public void display();
+
 
 }
